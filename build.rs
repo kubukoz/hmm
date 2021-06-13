d7f754caab250816774bf1f819497f7d94da4606
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-13 19:33:30 +0200

    Put completions in source (ignored) directory for nix

--- build.rs
+++ build.rs
@@ -3,9 +3,7 @@ use std::{env::var, fs::create_dir_all, path::Path};
 include!("src/cli.rs");
 
 fn main() {
-    let target = Path::new(&var("OUT_DIR").expect("OUT_DIR not specified"))
-        .join("completions")
-        .join("zsh");
+    let target = Path::new("completions").join("zsh");
 
     create_dir_all(&target).expect("Couldn't create directories");
 
